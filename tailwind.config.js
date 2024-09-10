5189e51c75efa22261eae26632fc74fba0a2030c
Federico Petrocchi <petrocchi.federico@gmail.com>
2024-09-10 15:54:45 +0200

    aggiunta progetto calcolatrice

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,css}"],
+  theme: {
+    extend: {
+      colors:{
+        'primary' : '#22252d', 
+        'secondary': '#292d36', 
+        'operators' : '#f56b6a', 
+        'numbers' : '#fefefe',
+        'functions' : '#25d7bf',
+        'homeButton' : '#212529',
+        'bgHoverButton' : '#f8f9fa',
+      }, 
+      fontFamily:{
+        'display' : "'Roboto', sans-serif",
+        'h1Button' : "'Lexend', sans-serif"
+      },
+
+      dropShadow: {
+        '4xl': '0px 16px 10px #000000'
+      }
+    },
+
+  },
+  plugins: [],
+}
