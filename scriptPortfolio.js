f11b62913885c02453edac11fb07f1365bd3e4a0
Federico Petrocchi <petrocchi.federico@gmail.com>
2024-09-10 16:08:16 +0200

    problemi importazione progetto calcolatrice

---
+++ scriptPortfolio.js
@@ -0,0 +1,25 @@
+// al click sui nav-link vai alla sezione corrispondente
+let navLinks = document.querySelectorAll('a[href^="#"]'); //vai a catturare tutti gli a che hanno attribtuo href che comincia con "#"
+
+//funzione per scrollare a sezioni
+
+console.log(navLinks);
+
+
+navLinks.forEach(link => { //cicla tutti i link
+        let hash = link.hash //tramite .hash recupero il contenuto in href="questoContenuto"
+        link.addEventListener("click", (e) => { //al click sul link vado a fare...
+            e.preventDefault(); //con preventDefault() disattivo il comportamento default dell'anchor (a)
+            gsap.to(window, {
+            duration: 0.2,
+            delay: 0.2,
+                scrollTo:{
+                    y: hash //scrolla fino alla sezione dedicata
+                }
+            });
+        });
+    })
+
+
+//libreria aos
+AOS.init();
